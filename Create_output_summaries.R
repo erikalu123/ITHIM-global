8f02a7aa19a4c31041fd5e59a85b25f34aa047a5
usr110 <m.aliabbas@gmail.com>
2024-05-08 12:06:27 +0100

    Remove code to create SHA specific summary docs. Fix #153

--- Create_output_summaries.R
+++ Create_output_summaries.R
@@ -25,22 +25,8 @@ if (!exists("output_version")){
 # create summary tables for AP and PA
 rmarkdown::render('summary_tables_PA_AP.Rmd', params = list(output_version = output_version))
 
-# rename output .html files
-file.rename('summary_tables_PA_AP.html', paste0('summary_tables_PA_AP_',output_version,'.html'))
-
-
-
 # create summary tables
 rmarkdown::render('summary_tables.Rmd', params = list(output_version = output_version))
 
-# rename output .html files
-file.rename('summary_tables.html', paste0('summary_tables_',output_version,'.html'))
-
-
-
 # create injury summary tables
 rmarkdown::render('injury_tables.Rmd', params = list(output_version = output_version))
-
-# rename output .html files
-file.rename('injury_tables.html', paste0('injury_tables_',output_version,'.html'))
-
